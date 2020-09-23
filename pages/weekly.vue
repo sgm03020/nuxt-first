48e7aa00d5f007b45cc714c0e75f4c7d2efde189
Tsuyoshi Koike <airtk2000@gmail.com>
2020-09-24 01:15:00 +0900

    change WeeklyTable to WeeklyGoodTable

--- pages/weekly.vue
+++ pages/weekly.vue
@@ -7,22 +7,22 @@
   <div class="container">
     <!-- <WeeklyBase /> -->
     <!-- <ItemForHour /> -->
-    <!-- <WeeklyTable /> -->
-    <WeeklyTest />
+    <WeeklyGoodTable />
+    <!-- <WeeklyTest /> -->
   </div>
 </template>
 
 <script>
 import WeeklyBase from '@/components/WeeklyBase'
 import ItemForHour from '@/components/ItemForHour'
-import WeeklyTable from '@/components/WeeklyTable'
+import WeeklyGoodTable from '@/components/WeeklyGoodTable'
 import WeeklyTest from '@/components/WeeklyTest'
 
 export default {
   components: {
     WeeklyBase,
     ItemForHour,
-    WeeklyTable,
+    WeeklyGoodTable,
     WeeklyTest,    
   },
 }
